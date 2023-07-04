dcca872d195aa1f5251df7e8fe8436b44f6a9f23
Ermolenko Elena <leo11_94@mail.ru>
2023-07-04 21:01:31 +0300

    ДЗ

---
+++ 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+ bool IsPalindrome( int number );
+ {
+    if (number < 0)
+    {
+        return false;
+    }
+
+    int RevNumber = 0;
+    int TempSourseNumber = number;
+    while (TempSourseNumber != 0)
+    {
+        int temp = TempSourseNumber % 10 ;
+        RevNumber = (RevNumber * 10) + temp;
+        TempSourseNumber = TempSourseNumber / 10;
+    }
+
+
+    if (RevNumber == number)
+       return true;
+
+    return false;
+        
+}
+// ---------------------------------------------
+Console.WriteLine(IsPalindrome (number: 12321));
